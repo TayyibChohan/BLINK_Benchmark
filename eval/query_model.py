8606270c831026954329b878e7c99c03ed07bd79
TayyibCh <tayyib.chohan@korotu.com>
2025-01-07 23:36:06 -0800

    extra line

--- eval/query_model.py
+++ eval/query_model.py
@@ -72,7 +72,7 @@ def query_llava(image_urls, question, conv_template="llava_llama_3"):
 model = Qwen2VLForConditionalGeneration.from_pretrained(
     "Qwen/QVQ-72B-Preview",
     # "Qwen/Qwen2-VL-2B-Instruct",
-    torch_dtype="auto",
+    # torch_dtype="auto",
     torch_dtype=torch.bfloat16,
     attn_implementation="flash_attention_2",
     device_map="auto",
