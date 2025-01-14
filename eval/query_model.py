eb49ec4399169d6d3c65861d1ca79e6a63561e23
TayyibCh <tayyib.chohan@korotu.com>
2025-01-14 01:03:30 -0800

    removed some stuff

--- eval/query_model.py
+++ eval/query_model.py
@@ -184,7 +184,7 @@ def query_qwenvl2(image_paths, prompt, retry=10):
             {
                 "prompt": prompt,
                 "multi_modal_data": {
-                    "image": image_inputs,
+                    "image": image_inputs
                     # "video": video_inputs
                 },
             },
