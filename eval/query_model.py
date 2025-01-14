2ff4906668199bb5c2d1141b662abfbcccab2f97
TayyibCh <tayyib.chohan@korotu.com>
2025-01-14 01:38:00 -0800

    big model

--- eval/query_model.py
+++ eval/query_model.py
@@ -80,9 +80,9 @@ def query_llava(image_urls, question, conv_template="llava_llama_3"):
 # )
 model_name = "Qwen/Qwen2-VL-7B-Instruct"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-sampling_params = SamplingParams(temperature=0.0, max_tokens=2048, stop_token_ids=None)
+sampling_params = SamplingParams(temperature=0.0, stop_token_ids=None)
 llm = LLM(model_name, 
-          max_model_len=32768,
+        #   max_model_len=32768
           max_num_seqs=5)
 
 min_pixels = 256*28*28
