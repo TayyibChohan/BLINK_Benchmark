8cf4f7a67eb7e3373755251413139d607cb0bd75
TayyibCh <tayyib.chohan@korotu.com>
2025-01-14 00:46:23 -0800

    typo

--- eval/query_model.py
+++ eval/query_model.py
@@ -178,21 +178,21 @@ def query_qwenvl2(image_paths, prompt, retry=10):
         # try:
             base64_images = [encode_image(image_path) for image_path in image_paths]
             messages = generate_qwen_vl2_message(base64_images, prompt)
-            text = processor.apply_chat_template(messages)
+            prompt = processor.apply_chat_template(messages, tokenize = False)
             # print(text)
             image_inputs, video_inputs = process_vision_info(messages)
-            inputs = processor(
-                text=[text],
-                images=image_inputs,
-                videos=video_inputs,
-                padding=True,
-                tokenize=False,
-            )
+            # inputs = processor(
+            #     text=[text],
+            #     images=image_inputs,
+            #     videos=video_inputs,
+            #     padding=True,
+            #     return_tensors="pt"
+            # )
 
 
             outputs = llm.generate(
             {
-                "prompt": inputs,
+                "prompt": prompt,
                 "multi_modal_data": {
                     "image": image_inputs,
                     "video": video_inputs,
