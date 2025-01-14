e179cb375c007641921af10ef9afb97b15eeed12
TayyibCh <tayyib.chohan@korotu.com>
2025-01-13 23:21:41 -0800

    removed some stuff

--- eval/query_model.py
+++ eval/query_model.py
@@ -186,7 +186,7 @@ def query_qwenvl2(image_paths, prompt, retry=10):
             #     padding=True,
             #     return_tensors="pt",
             # )
-            inputs = inputs.to("cuda")
+            # inputs = inputs.to("cuda")
             generated_ids = tokenizer.generate([text], sampling_params)
             generated_ids_trimmed = [
                 out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
