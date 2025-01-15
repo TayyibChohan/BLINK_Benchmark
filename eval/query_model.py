4d7c450543ef2d88bb0d583c4a3c00b26358238f
TayyibCh <tayyib.chohan@korotu.com>
2025-01-14 16:57:54 -0800

    big model

--- eval/query_model.py
+++ eval/query_model.py
@@ -40,9 +40,10 @@ model_name = "Qwen/Qwen2-VL-7B-Instruct"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 sampling_params = SamplingParams(temperature=0.0, stop_token_ids=None)
 llm = LLM(model_name,
-        max_model_len=32768,
-        limit_mm_per_prompt={"image": 4}
-          )
+        max_model_len=32768 if process_vision_info is None else 4096,
+        max_num_seqs=5,
+        limit_mm_per_prompt={"image": max_images},
+        )
 
 min_pixels = 256*28*28
 max_pixels = 1280*28*28
